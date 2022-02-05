384a46c01cb55668050d4506307481db84f4bfdb
Jurytan Nguyen <jnguyen7410@gmail.com>
2022-02-05 06:22:07 -0500

    Adding the basic structure

---
+++ lib/models/weather.dart
@@ -0,0 +1,10 @@
+class Weather {
+  String city;
+  DateTime expectedEta;
+  
+}
+
+enum WeatherConditions {
+  SNOW, FOG, RAIN, CLOUDY, SUNNY, PARTLY_CLOUDY,
+  HURRICANE, MONSOON, TORNADO, NUCLEAR_WINTER
+}
